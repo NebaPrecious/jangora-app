74bbe93bcad2a7cfaebd6c4161efaf50f6904244
neba precious <preciousngehneba@gmail.com>
2026-06-18 16:36:21 +0100

    feat: create authentication and onboarding pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,24 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'splash',
+    loadComponent: () => import('./features/auth/splash/splash.page').then( m => m.SplashPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'onboarding',
+    loadComponent: () => import('./features/onboarding/onboarding/onboarding.page').then( m => m.OnboardingPage)
+  },
 ];
