d0b96cd2e43776ae22bd5565b42acbd997a89d42
neba precious <preciousngehneba@gmail.com>
2026-06-18 17:10:10 +0100

    feat: create animated splash screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,32 +3,47 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'splash',
     pathMatch: 'full',
   },
-  {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
-  },
+
   {
     path: 'splash',
-    loadComponent: () => import('./features/auth/splash/splash.page').then( m => m.SplashPage)
+    loadComponent: () =>
+      import('./features/auth/splash/splash.page').then(
+        (m) => m.SplashPage,
+      ),
   },
-  {
-    path: 'login',
-    loadComponent: () => import('./features/auth/login/login.page').then( m => m.LoginPage)
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
-  },
+
   {
     path: 'onboarding',
-    loadComponent: () => import('./features/onboarding/onboarding/onboarding.page').then( m => m.OnboardingPage)
+    loadComponent: () =>
+      import('./features/onboarding/onboarding/onboarding.page').then(
+        (m) => m.OnboardingPage,
+      ),
+  },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.page').then(
+        (m) => m.LoginPage,
+      ),
+  },
+
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register.page').then(
+        (m) => m.RegisterPage,
+      ),
+  },
+
+  {
+    path: 'forgot-password',
+    loadComponent: () =>
+      import(
+        './features/auth/forgot-password/forgot-password.page'
+      ).then((m) => m.ForgotPasswordPage),
   },
 ];
