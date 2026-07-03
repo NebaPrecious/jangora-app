d8a4f5582544ead4f39ba585600ec943ffa323a1
neba precious <preciousngehneba@gmail.com>
2026-07-03 13:47:06 +0100

    feat(auth): complete authentication and onboarding interfaces

--- src/app/features/auth/forgot-password/forgot-password.page.ts
+++ src/app/features/auth/forgot-password/forgot-password.page.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonButton,
+  IonInput,
+  IonSpinner
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-forgot-password',
+  standalone: true,
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonButton,
+    IonInput,
+    IonSpinner
+  ]
 })
-export class ForgotPasswordPage implements OnInit {
+export class ForgotPasswordPage {
 
-  constructor() { }
+  email = '';
+  loading = false;
+  emailSent = false;
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  sendResetLink() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      this.emailSent = true;
+    }, 1500);
   }
 
+  backToLogin() {
+    this.router.navigateByUrl('/login');
+  }
 }
